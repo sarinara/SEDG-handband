33808b8f7331d57decc11a367d57e054049e3b86
Rishi Kothari <hey@rishi.cx>
2023-08-31 09:52:21 +0900

    Initial Commit

---
+++ SEDG-handband/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SEDG-handband
+//
+//  Created by Rishi Kothari on 2023-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SEDG-handband/SEDG_handbandApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEDG_handbandApp.swift
+//  SEDG-handband
+//
+//  Created by Rishi Kothari on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SEDG_handbandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
